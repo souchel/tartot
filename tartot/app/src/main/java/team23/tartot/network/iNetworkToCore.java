4db05fe5c2defd234e38908b4257d2fa575d8bfc
Guillaume Weghsteen <guillaume.weghsteen@gmail.com>
2018-02-20 14:57:22 +0100

    echange des noms d'interfaces

--- tartot/app/src/main/java/team23/tartot/network/iNetworkToCore.java
+++ tartot/app/src/main/java/team23/tartot/network/iNetworkToCore.java
@@ -1,79 +1,33 @@
 package team23.tartot.network;
-import java.util.List;
 
 import team23.tartot.core.Announce;
 import team23.tartot.core.Card;
 import team23.tartot.core.Player;
-import team23.tartot.core.iPlayer;
 
 /**
- * Created by neuracr on 01/02/2018.
- * iNetwork interface definition v0.1
+ * Created by neuracr on 20/02/18.
  */
 
 /**
- * interface that manages the connection between players, lobby creation.
- * Everything that needs internet access.
+ * This interface should be implemented by the class of the core package connected to the network class.
+ * The callbacks defined in this interface will be called by the network class to relay events triggered by other players (coming from the internet).
  */
 public interface iNetworkToCore {
     /**
-     * creates a new empty lobby
+     * callback raised when an invitation is received from another player.
      */
-    public void createLobby();
+    public void onInvitationReceived(
+            //to define
+    );
 
     /**
-     * Leaves the current lobby. If we were lobby leader, assign leader role
-     * to a new player in lobby. Deletes the lobby otherwise.
-     */
-    public void leaveLobby();
-
-
-    /**
-     * Invite the player in parameter in the lobby occupied by us.
-     * @param player the player to invite
-     */
-    public void inviteFriend(iPlayer player);
-
-    /**
-     * Start a new game with the players in the lobby.
-     */
-    public void startGame();
-
-    /**
-     * Leave the current game and notify the other players
-     */
-    public void leaveGame();
-
-    /**
-     * callback to treat incomming data when in game (card played, announce, ...)
-     * @param data
-     */
-    public void onInGameDataReceive(Object data);
-
-    /**
-     * callback to treat incomming data when in lobby (player left the lobby, game starts, ...)
-     */
-    public void onInLobbyDataReceive();
-
-
-    /**
-     * deal the cards to the other players.
-     * Raise an exception if the player is not in game.
-     * No other verification is done.
-     * @param destination: the player we send the cards to
-     * @param cards: the cards delt
-     */
-    public void dealCards(Player destination, Card[] cards);
-
-    /**
-     * notify other players the card we chosed to play
+     * callback raised after the connection manager is triggered by a player that plays a card
+     * @param player
      * @param card
      */
-    public void playCard(Card card);
+    public void onPlayCard(Player player, Card card);
 
-    /**
-     * Inform other players of an announce. Raise an exception if we are not in game
-     * @param announce: the announce to broadcast
-     */
-    public void announce(Announce announce);
+    public void onCardsDelt(Card[] cards);
+
+    public void onAnnounce(Player player, Announce announce);
 }
