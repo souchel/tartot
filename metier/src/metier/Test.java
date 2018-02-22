9dec9742c28d2a2afcd7f41f608b17498a32eb95
Laure Souche <lsouche@hku.hk>
2018-02-22 10:00:00 +0100

    modif methode de hugo

--- metier/src/metier/Test.java
+++ metier/src/metier/Test.java
@@ -4,8 +4,8 @@ import java.io.*;
 public class Test {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Une carte est creee");
-		Card card = new Card(Suit.DIAMOND, 14);
-		System.out.println("valeur : "+card.getValue()+" Couleur : "+card.getSuit());
+		Card card = new Card(Suit.TRUMP, 22);
+		System.out.println("valeur : "+card.getValue()+" soit "+card.valueToString()+ " Couleur : "+card.getSuit());
 		System.out.println("cette carte a pour valeur : "+ card.pointValue());
 		System.out.println("");
 		System.out.println("Un joueur est cree");
