73ede3451a8c6e295d98b808686ca66701c9d64c
Hsb511 <hugo-selle@hotmail.fr>
2018-02-23 17:31:30 +0100

    modification du visuel de ActivityFirstLog : ajout de tv, et & button et de modification de leur texte dans strings

--- tartot/app/src/main/java/team23/tartot/FirstLogActivity.java
+++ tartot/app/src/main/java/team23/tartot/FirstLogActivity.java
@@ -8,6 +8,8 @@ public class FirstLogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first_log);
     }
+
+
 }
