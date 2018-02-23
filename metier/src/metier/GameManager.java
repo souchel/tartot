0e5fc8d795001b8a5103cebaa287ccabfc13a82d
keeganpa <keeganpa76@hotmail.fr>
2018-02-23 04:32:41 +0100

    création

---
+++ metier/src/metier/GameManager.java
@@ -0,0 +1,18 @@
+package metier;
+
+public class GameManager {
+	private int nbTurn;
+	
+	
+	private void nextDealer() {
+		
+	}
+	
+	private Player getPlayerAtPosition(int i) {
+		
+	}
+	
+	public void startPlaying() {
+		
+	}
+}
