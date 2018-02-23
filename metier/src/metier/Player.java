557dcdee9f959eae95a9fbb4aa643ecfb9aa6b7a
keeganpa <keeganpa76@hotmail.fr>
2018-02-23 13:12:46 +0100

    ajout methodes d'existance de carte dans le deck de player

--- metier/src/metier/Player.java
+++ metier/src/metier/Player.java
@@ -3,12 +3,14 @@ package metier;
 public class Player {
 	private String username;
 	private Team team;
+	private Deck deck;
 	
 	public Player(String username)
 	{
 		this.username = username;
 		//quand les players sont crees ils n ont pas encore d equipe d ou ils sont dans l equipe none
 		team = Team.NONE;
+		deck = new Deck();
 	}
 	public String getUsername()
 	{
@@ -17,5 +19,34 @@ public class Player {
 	public Team getTeam()
 	{
 		return team;
+	}
+	
+	
+	
+	
+	
+	//methods to check existing card in deck
+	public boolean haveSuit(Suit suit) {
+		for (Card card : deck.getCardList()) {
+			if (card.getSuit() == suit) {
+				return true;
+			}
+		} return false;
+	}
+	public boolean haveTrump() {
+		for (Card card : deck.getCardList()) {
+			if (card.getSuit() == Suit.TRUMP) {
+				return true;
+			}
+		} return false;
+	}
+	public boolean haveHigherTrump(int value) {
+		for (Card card : deck.getCardList()) {
+			if (card.getSuit() == Suit.TRUMP) {
+				if (card.getValue() > value) {
+					return true;
+				}
+			}
+		} return false;
 	}
 }
