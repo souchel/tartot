d0b0aceefb4c476761b295d04f7131d99a98f679
Hsb511 <hugo-selle@hotmail.fr>
2018-02-23 17:43:31 +0100

    création du onCLickListener du bouton de FirstLogActivity avec Intent pour aller dans MainMenuActovoty, instanciation du Player en commentaire

--- tartot/app/src/main/java/team23/tartot/FirstLogActivity.java
+++ tartot/app/src/main/java/team23/tartot/FirstLogActivity.java
@@ -1,7 +1,10 @@
 package team23.tartot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstLogActivity extends AppCompatActivity {
 
@@ -9,7 +12,17 @@ public class FirstLogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_log);
-    }
 
+        final Button firstLogButton = findViewById(R.id.button_first_log);
+        firstLogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToMainMenuIntent = new Intent(FirstLogActivity.this, MainMenuActivity.class);
+                //Player newPlayer = Player(username);
+                //goToMainMenuIntent.putExtra(newPlayer);
+                startActivity(goToMainMenuIntent);
+            }
+        });
+    }
 
 }
