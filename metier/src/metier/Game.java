783016793006ab65bc2a56f20ebb3b13886ad5b5
Laure Souche <lsouche@hku.hk>
2018-02-23 15:19:07 +0100

    fin distribution

--- metier/src/metier/Game.java
+++ metier/src/metier/Game.java
@@ -1,4 +1,6 @@
 package metier;
+import java.util.Random;
+
 
 public class Game {
 	//the announce
@@ -11,21 +13,21 @@ public class Game {
 	Deck deck;
 	Player[] players;
 	OnGoingFold onGoingFold ; 
-	int numberOfPlayers;
 	Player dealer ;
 	int indexDealer = 0 ;
+	Deck chien;
 	
 	public Game(String[] usernames)
 	{
-		numberOfPlayers = usernames.length;
 		deck = new Deck();
 		onGoingFold = new OnGoingFold();
-		players = new Player[numberOfPlayers];
-		for (int i = 0 ; i < numberOfPlayers; i++)
+		players = new Player[usernames.length];
+		for (int i = 0 ; i < usernames.length; i++)
 		{
 			players[i] = new Player(usernames[i]);
 		}
 		dealer = players[indexDealer];
+		chien = new Deck();
 	}
 	public void initializeGame()
 	{
@@ -51,15 +53,41 @@ public class Game {
 			}
 		}
 	}
-	
+	public boolean alreadyInArray(int[] array, int number)
+	//return true if number is already in the array of int called array and false if not
+	{
+		for (int i = 0 ; i < array.length ; i ++)
+		{
+			if (array[i]==number)
+				return true ;
+		}
+		return false;
+	}
 	public void distribute()
 	{
+		//on cherche a savoir a quel moment on va mettre des cartes dans le chien
+		//On cree une liste de moments qui ne peuvent pas exister
+		int[] indexes = {25,25,25,25,25,25};
+		Random rndGenerator = new Random();
+		//on pioche 6 entiers entre 0 et 22 sachant qu'on distribue 3 fois 24 cartes mais qu on ne 
+		//peut pas mettre la derniere carte dans le chien
+		for (int j = 0 ; j < 6 ; j ++)
+		{
+			int index = rndGenerator.nextInt(23);
+			//On pioche jusqu a avoir un index different de ceux deja dans la liste
+			while (alreadyInArray(indexes, index))
+				index = rndGenerator.nextInt(23);
+			indexes[j] = index ;
+		}
+		//On distribue les cartes 3 par 3 aux 4 joueurs
 		for (int i = 1; i <= 24 ; i++)
 		{
+			//on donne 3 cartes au joueur i modulo 4
 			for (int j = 1 ; j <= 3 ; j++)
-			{
-				players[(i/numberOfPlayers)-1].getDeck().addCard(deck.removeCardByIndex(0));
-			}
+				players[(i/4)-1].getDeck().addCard(deck.removeCardByIndex(0));
+			//si c est l un des tours ou on est cense mettre dans le chien, on met une carte dans le chien
+			if (alreadyInArray(indexes, i))
+				chien.addCard(deck.removeCardByIndex(0));
 		}
 	}
 	public OnGoingFold getOnGoingFold()
