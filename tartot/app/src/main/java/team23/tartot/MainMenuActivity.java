0b3da44ba56b3772b79742c4f52c7ae464b9b0b0
tcantin <thomascantin7@gmail.com>
2018-02-24 20:03:49 +0100

    Ajoute la gestion des invitations

--- tartot/app/src/main/java/team23/tartot/MainMenuActivity.java
+++ tartot/app/src/main/java/team23/tartot/MainMenuActivity.java
@@ -17,11 +17,17 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.multiplayer.Invitation;
+import com.google.android.gms.games.multiplayer.Multiplayer;
+import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.common.SignInButton;
 
 public class MainMenuActivity extends AppCompatActivity {
+    private static final int RC_INVITATION_INBOX = 9008;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,7 @@ public class MainMenuActivity extends AppCompatActivity {
 
         final TextView welcomeTextView = findViewById(R.id.text_view_welcome_main_menu);
         final Button goToLobbyButton = findViewById(R.id.text_view_go_to_lobby);
+        final Button goToInvitationInbox = findViewById(R.id.button_go_to_invitation_inbox);
 
         String welcome = welcomeTextView.getText().toString() + " Hsb511"; // + getExtra.getUsername();
         welcomeTextView.setText(welcome);
@@ -42,6 +49,54 @@ public class MainMenuActivity extends AppCompatActivity {
                                            }
         );
 
-
+        goToInvitationInbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showInvitationInbox();
+            }
+        });
     }
+
+    /**
+     * This method will display Google default UI for listing invitations.
+     */
+    private void showInvitationInbox() {
+        Games.getInvitationsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .getInvitationInboxIntent()
+                .addOnSuccessListener(new OnSuccessListener<Intent>() {
+                    @Override
+                    public void onSuccess(Intent intent) {
+                        startActivityForResult(intent, RC_INVITATION_INBOX);
+                    }
+                });
+    }
+
+    /**
+     * Called when the player gets back to the main menu from the invitation inbox.
+     * If he has accepted an invitation, he should be redirected to the concerned room.
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_INVITATION_INBOX) {
+            if (resultCode != Activity.RESULT_OK) {
+                // Canceled or some error.
+                return;
+            }
+
+            Invitation invitation = data.getExtras().getParcelable(Multiplayer.EXTRA_INVITATION);
+            if (invitation != null) {
+                RoomConfig.Builder builder = RoomConfig.builder(mRoomUpdateCallback)
+                        .setInvitationIdToAccept(invitation.getInvitationId());
+                mJoinedRoomConfig = builder.build();
+                Games.getRealTimeMultiplayerClient(thisActivity,
+                        GoogleSignIn.getLastSignedInAccount(this))
+                        .join(mJoinedRoomConfig);
+                // prevent screen from sleeping during handshake
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        }
+    }
+
+
 }
