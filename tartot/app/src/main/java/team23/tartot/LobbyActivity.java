bfeca5818a0bcd62a2318e6674b49af6809714a2
Guillaume Weghsteen <guillaume.weghsteen@gmail.com>
2018-02-25 23:43:38 +0100

    tentative de rattrappage de ce fiasco

--- tartot/app/src/main/java/team23/tartot/LobbyActivity.java
+++ tartot/app/src/main/java/team23/tartot/LobbyActivity.java
@@ -8,7 +8,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.MalformedJsonException;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -68,11 +67,7 @@ public class LobbyActivity extends AppCompatActivity {
         button_leave_lobby.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mJoinedRoom != null && roomID != null) {
-                    RTMClient.leave(mJoinedRoom, roomID);
-                    mJoinedRoom = null;
-                    roomID = null;
-                }
+                RTMClient.leave(mJoinedRoom, roomID);
 
                 // Sending to main menu again
                 Intent goToMainMenuIntent = new Intent(LobbyActivity.this, MainMenuActivity.class);
@@ -115,6 +110,14 @@ public class LobbyActivity extends AppCompatActivity {
         //createLobby(); //commenté car il vaut mieux créer le lobby après invitation de joueurs ou auto match
     }
 
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i("debug", "LobbyActivity.onRestoreInstanceState");
+        currentRoom = savedInstanceState.getParcelable("currentRoom");
+        roomID = currentRoom.getRoomId();
+
+    }
 
     /**
      * This method will display Google default UI for listing invitations.
@@ -251,7 +254,6 @@ public class LobbyActivity extends AppCompatActivity {
                 getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
                 joinTask.addOnCompleteListener(new OnCompleteListener<Void>() {
-
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
@@ -288,16 +290,12 @@ public class LobbyActivity extends AppCompatActivity {
                 // continue to connect in the background.
 
                 //On fait quoi nous ?
-                // Minimize ?
             } else if (resultCode == GamesActivityResultCodes.RESULT_LEFT_ROOM) {
                 // player wants to leave the room.
                 Games.getRealTimeMultiplayerClient(this,
                         GoogleSignIn.getLastSignedInAccount(this))
                         .leave(mJoinedRoom, currentRoom.getRoomId());
                 getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-                if (RTMClient != null && mJoinedRoom != null && roomID != null)
-                    this.RTMClient.leave(mJoinedRoom, roomID);
             }
         }
 
@@ -437,15 +435,8 @@ public class LobbyActivity extends AppCompatActivity {
      * Leaves the current lobby. If we were lobby leader, assign leader role
      * to a new player in lobby. Deletes the lobby otherwise.
      */
-    public void leaveLobby() {
-        if (RTMClient != null && mJoinedRoom != null && roomID != null)
-            this.RTMClient.leave(mJoinedRoom, roomID);
-    }
+    public void leaveLobby() {}
 
-    /**
-     * Deal with important parameters to save between stop / resume activityLobby
-     * @param savedInstanceState
-     */
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
@@ -453,21 +444,5 @@ public class LobbyActivity extends AppCompatActivity {
         savedInstanceState.putParcelable("currentRoom", this.currentRoom);
     }
 
-    @Override
-    public void onRestoreInstanceState(Bundle savedInstanceState){
-        super.onRestoreInstanceState(savedInstanceState);
-        Log.i("debug", "LobbyActivity.onRestoreInstanceState");
-        currentRoom = savedInstanceState.getParcelable("currentRoom");
-        roomID = currentRoom.getRoomId();
-
-    }
-
-    @Override
-    public void onDestroy() {
-        if (RTMClient != null && mJoinedRoom != null && roomID != null)
-            this.RTMClient.leave(mJoinedRoom, roomID);
-
-        super.onDestroy();
-    }
 
 }
