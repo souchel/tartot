cc56bc2799f64aa0f45d72e9b552629a0206c0cb
tcantin <thomascantin7@gmail.com>
2018-02-25 17:19:47 +0100

    Merge et modifications mineures

--- tartot/app/src/main/java/team23/tartot/LobbyActivity.java
+++ tartot/app/src/main/java/team23/tartot/LobbyActivity.java
@@ -2,14 +2,17 @@ package team23.tartot;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -424,7 +427,7 @@ public class LobbyActivity extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        Log.i("debug", "LobbyActivity.onSaveInstanceState");
+
         savedInstanceState.putParcelable("currentRoom", this.currentRoom);
     }
 
