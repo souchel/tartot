b54642a6e7f886c8f1037d61c7723a019c8f30f6
Hsb511 <hugo-selle@hotmail.fr>
2018-02-28 19:06:24 +0100

    test des cartes dans gameActivity, il faut rajouter dans le build.gradle de l'app dans android{defaultConfig : vectorDrawables.useSupportLibrary = true

--- tartot/app/src/main/java/team23/tartot/GameActivity.java
+++ tartot/app/src/main/java/team23/tartot/GameActivity.java
@@ -3,14 +3,28 @@ package team23.tartot;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import team23.tartot.core.Player;
 
 public class GameActivity extends AppCompatActivity {
 
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        ImageButton imageButtonColor = findViewById(R.id.imageButtonColor);
+        imageButtonColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tvTest = findViewById(R.id.textViewTest);
+                tvTest.setText("couleur");
+                Log.i("onCLick", "carreau");
+            }
+        });
+    }
 }
