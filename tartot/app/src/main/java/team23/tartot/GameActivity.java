4ba34a4d07bab1ac6af057aba5935d6c5606f837
Hsb511 <hugo-selle@hotmail.fr>
2018-03-10 12:21:52 +0100

    test de distribution de paquet de cartes. Ca met 3sec à s'afficher. Les trèfles ne s'affichent en pique c'est normal mais les carreaux ne s'affichent pas en carreaux. Les cartes vides sont des atouts. To Do cf TODO

--- tartot/app/src/main/java/team23/tartot/GameActivity.java
+++ tartot/app/src/main/java/team23/tartot/GameActivity.java
@@ -20,16 +20,23 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
+import team23.tartot.core.Card;
+import team23.tartot.core.Deck;
+import team23.tartot.core.GameManager;
 import team23.tartot.core.Player;
+import team23.tartot.core.Suit;
 
 public class GameActivity extends AppCompatActivity {
-    final private static int CARD_WIDTH = 80;
-    final private static int CARD_HEIGHT = 160;
-    final private static int NUMBER_OF_CARDS = 30;
+    final private static int CARD_WIDTH = 100;
+    final private static int CARD_HEIGHT = 200;
+    final private static int NUMBER_OF_CARDS = 18;
 
+    protected Deck deck = new Deck();
     private int cardNumber = 0;
+    protected ArrayList<Card> hand = new ArrayList<Card>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,76 +44,113 @@ public class GameActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        initializeDeck();
+        deck.shuffle();
+
+        for(int i = 0; i < NUMBER_OF_CARDS; i++) {
+            hand.add(deck.getCardList().get(i));
+        }
 
         /*/ ClickListener of a button that should create (graphically) a Card with a FrameLayout with inside it imageViews and button /*/
         findViewById(R.id.test_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LinearLayout cardsUpLayout = findViewById(R.id.cards_up_layout);
-                LinearLayout cardsDownLayout = findViewById(R.id.cards_down_layout);
-                cardsDownLayout.setPadding(0,0,0,-60);
-
-                //We create a (in the future several) FrameLayout for one Card
-                FrameLayout cardFL = new FrameLayout(getApplicationContext());
-
-                //WE DEAL WITH THE BACKGROUND
-                Bitmap cardBGBP = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.card_border);
-                Bitmap cardBGResizedBP = getResizedBitmap(cardBGBP, CARD_WIDTH, CARD_HEIGHT);
-                ImageView cardBackgroundIV = new ImageView(getApplicationContext());
-                cardBackgroundIV.setImageBitmap(cardBGResizedBP);
-
-
-                ///WE DEAL WITH THE VALUE
-                //We create an int for the card value (FOR THE TEST)
-                Random rand = new Random();
-                final int value = rand.nextInt(9)+1;
-                final String color = "coeur";
-
-                //WE DEAL WITH THE COLOR
-                Bitmap cardColorBP = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.card_color_hearts);
-                Bitmap cardColorResizedBP = getResizedBitmap(cardColorBP, CARD_WIDTH, CARD_HEIGHT);
-                ImageView cardColorIV = new ImageView(getApplicationContext());
-                cardColorIV.setImageBitmap(cardColorResizedBP);
-
-
-                //We create the 2 textViews, one for the value up and one for the one down
-                TextView cardValueUpTV = createTVforValue(value, true, true);
-                TextView cardValueDownTV = createTVforValue(value, true, false);
-
-                //We create a button for the action
-                Button cardButton = new Button(getApplicationContext());
-                cardButton.setBackgroundColor(getResources().getColor(R.color.transparent));
-                cardButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        TextView testTV = findViewById(R.id.textViewTest);
-                        testTV.setText(Integer.toString(value)+" de "+ color);
-                    }
-                });
-
-                // we add the image view
-                cardFL.addView(cardBackgroundIV);
-                cardFL.addView(cardColorIV);
-                cardFL.addView(cardValueUpTV);
-                cardFL.addView(cardValueDownTV);
-                cardFL.addView(cardButton);
-
-                //we set Layout Parameters
-                FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(CARD_WIDTH,CARD_HEIGHT);
-                cardFL.setLayoutParams(lp);
-
-                //we add the frameLayout to the horizontal LinearLayout depending on the number of card already displayed
-                if (cardNumber < NUMBER_OF_CARDS/2) {
-                    cardsDownLayout.addView(cardFL);
-                } else if (cardNumber < NUMBER_OF_CARDS){
-                    cardsUpLayout.addView(cardFL);
-                } else {
-
-                }
-
-                cardNumber++;
+                onCardsAddedToPlayersDeck(hand);
             }
         });
+    }
+
+
+    public void onCardsAddedToPlayersDeck (ArrayList<Card> hand) { //it could be the distribution or the addition of the dog into the player's deck
+        LinearLayout cardsUpLayout = findViewById(R.id.cards_up_layout);
+        LinearLayout cardsDownLayout = findViewById(R.id.cards_down_layout);
+        cardsDownLayout.setPadding(0,0,0,-CARD_HEIGHT/2);
+
+        for (int j = 0; j < hand.size(); j++) {
+            Card currentCard = hand.get(j);
+            final String value = currentCard.valueToString();
+            final String suit = currentCard.getSuit().toString();
+
+            //We create a (in the future several) FrameLayout for one Card
+            FrameLayout cardFL = new FrameLayout(getApplicationContext());
+
+            //WE DEAL WITH THE BACKGROUND
+            Bitmap cardBGBP = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.card_border);
+            Bitmap cardBGResizedBP = getResizedBitmap(cardBGBP, CARD_WIDTH, CARD_HEIGHT);
+            ImageView cardBackgroundIV = new ImageView(getApplicationContext());
+            cardBackgroundIV.setImageBitmap(cardBGResizedBP);
+
+            //WE DEAL WITH THE COLOR
+            Bitmap cardColorBP = cardColorBP = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.card_color_spades);;
+            if (suit == "s") {
+                cardColorBP = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.card_color_spades);
+            } else if (suit == "h") {
+                cardColorBP = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.card_color_hearts);
+            } else if (suit == "d") {
+                cardColorBP = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.card_color_diamonds);
+            } else if (suit == "t") {
+                cardColorBP = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.card_border);
+            }
+
+            Bitmap cardColorResizedBP = getResizedBitmap(cardColorBP, CARD_WIDTH, CARD_HEIGHT);
+            ImageView cardColorIV = new ImageView(getApplicationContext());
+            cardColorIV.setImageBitmap(cardColorResizedBP);
+
+            //WE DEAL WITH THE VALUE
+            TextView cardValueUpTV = new TextView(getApplicationContext());
+            TextView cardValueDownTV = new TextView(getApplicationContext());
+            //We create the 2 textViews, one for the value up and one for the one down
+            if (suit != "t") {
+                cardValueUpTV = createTVforValue(value, suitIntoColor(suit), true);
+                cardValueDownTV = createTVforValue(value, suitIntoColor(suit), false);
+            } else if (suit == "t") {
+                //TODO MANAGE THE TRUMPS' DISPLAYING & THE 10 DISPLAYING
+            }
+
+            //We create a button for the action
+            Button cardButton = new Button(getApplicationContext());
+            cardButton.setBackgroundColor(getResources().getColor(R.color.transparent));
+            cardButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView testTV = findViewById(R.id.textViewTest);
+                    testTV.setText(value+" de "+ suit);
+                    //TODO PLACE THE CARD IN THE MIDDLE OF THE GAME (SO CREATE A LAYOUT IN THE MIDDLE OF THE GAME)
+                    //TODO REMOVE THE CARD FROM THE PLAYER'S HAND
+                }
+            });
+
+            // we add the image view
+            cardFL.addView(cardBackgroundIV);
+            cardFL.addView(cardColorIV);
+            cardFL.addView(cardValueUpTV);
+            cardFL.addView(cardValueDownTV);
+            cardFL.addView(cardButton);
+
+            //we set Layout Parameters
+            FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(CARD_WIDTH,CARD_HEIGHT);
+            cardFL.setLayoutParams(lp);
+
+            //we add the frameLayout to the horizontal LinearLayout depending on the number of card already displayed
+            if (cardNumber < NUMBER_OF_CARDS/2) {
+                cardsDownLayout.addView(cardFL);
+            } else if (cardNumber < NUMBER_OF_CARDS){
+                cardsUpLayout.addView(cardFL);
+            } else {
+
+            }
+
+            cardNumber++;
+        }
+
+    }
+
+    public boolean suitIntoColor (String suit) {
+        boolean color = true; //if true corresponds to red, false is black
+        if (suit == "s" || suit == "c") {
+            color = false;
+        }
+        return color;
     }
 
     public Bitmap getResizedBitmap (Bitmap bm, int newWidth, int newHeight) {
@@ -126,11 +170,11 @@ public class GameActivity extends AppCompatActivity {
         return resizedBitmap;
     }
 
-    public TextView createTVforValue(int value, boolean color, boolean position) {
+    public TextView createTVforValue(String value, boolean color, boolean position) {
         TextView cardValueTV = new TextView(getApplicationContext());
 
         //we set the card value
-        cardValueTV.setText(Integer.toString(value));
+        cardValueTV.setText(value);
 
         //we set the value color (red or black)
         if (color == true) { //true means red, false means black
@@ -148,6 +192,27 @@ public class GameActivity extends AppCompatActivity {
         cardValueTV.setPadding(6,-3,0,0);
         cardValueTV.setTextSize(10);
         return cardValueTV;
+    }
+
+    //FOR TEST ONLY, WE SHOULD USE A GAMEMANAGER
+    public void initializeDeck()
+    {
+        for (Suit suit : Suit.values())
+        {
+            int bound ;
+            if (suit ==  Suit.TRUMP)
+            {
+                bound = 22;
+            }
+            else
+            {
+                bound = 14;
+            }
+            for (int i = 1; i<=bound; i++)
+            {
+                deck.addCard(new Card(suit, i));
+            }
+        }
     }
 }
 
